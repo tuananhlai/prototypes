c5754543b34f45537349e2be0efb292b6c1a516e
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-08-28 16:29:22 +0700

    connection-pool: no pool

--- connection-pool/go.mod
+++ connection-pool/go.mod
@@ -1,3 +1,5 @@
 module github.com/tuananhlai/prototypes/connection-pool
 
 go 1.25.0
+
+require github.com/lib/pq v1.10.9
--- connection-pool/main.go
+++ connection-pool/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"database/sql"
 
+	_ "github.com/lib/pq"
+)
+
+func benchmarkNoPool() {
+	connStr := "postgres://postgres:postgres@localhost:5432/prototype?sslmode=disable"
+
+	for range 500 {
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		_, err = db.Query("SELECT 1")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	benchmarkNoPool()
 }
