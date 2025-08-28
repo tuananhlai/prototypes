99885b8cd8d8011e8bb5ecf18720594e79ee76c9
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-08-28 20:25:10 +0700

    wip

--- connection-pool/main.go
+++ connection-pool/main.go
@@ -2,27 +2,64 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"sync"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
+const (
+	connStr = "postgres://postgres:postgres@localhost:5432/prototype?sslmode=disable"
+)
+
+func benchmarkOneConnection() {
+	start := time.Now()
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.SetMaxOpenConns(1)
+
+	var wg sync.WaitGroup
+
+	for range 2 {
+		wg.Go(func() {
+			_, err = db.Query("SELECT 1")
+			if err != nil {
+				fmt.Printf("error executing query: %v", err)
+			}
+		})
+	}
+	wg.Wait()
+	fmt.Println(time.Since(start))
+}
+
 func benchmarkNoPool() {
-	connStr := "postgres://postgres:postgres@localhost:5432/prototype?sslmode=disable"
+	start := time.Now()
+	var wg sync.WaitGroup
 
 	for range 500 {
-		db, err := sql.Open("postgres", connStr)
-		if err != nil {
-			panic(err)
-		}
-		defer db.Close()
+		wg.Go(func() {
+			db, err := sql.Open("postgres", connStr)
+			if err != nil {
+				panic(err)
+			}
+			defer db.Close()
 
-		_, err = db.Query("SELECT 1")
-		if err != nil {
-			panic(err)
-		}
+			_, err = db.Query("SELECT 1")
+			if err != nil {
+				panic(err)
+			}
+		})
 	}
+	wg.Wait()
+	fmt.Println(time.Since(start))
 }
 
 func main() {
-	benchmarkNoPool()
+	benchmarkOneConnection()
 }
