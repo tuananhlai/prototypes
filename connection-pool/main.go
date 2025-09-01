1e97f82eac23d5ef514ee198b5e5c0b687d553f2
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-09-01 14:55:20 +0700

    add connection pool

--- connection-pool/main.go
+++ connection-pool/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -14,20 +15,54 @@ const (
 	connStr = "postgres://postgres:postgres@localhost:5432/prototype?sslmode=disable"
 )
 
-func benchmarkWithPool() {
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatalf("error opening new connection: %v", err)
-		return
-	}
-	defer db.Close()
+type ConnectionPool struct {
+	db       *sql.DB
+	connChan chan *sql.Conn
+}
 
-	db.SetMaxOpenConns(10)
+func NewConnectionPool(dsn string) (*ConnectionPool, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	connChan := make(chan *sql.Conn, 10)
+
+	ctx := context.Background()
+	for range 10 {
+		conn, err := db.Conn(ctx)
+		if err != nil {
+			return nil, err
+		}
+		connChan <- conn
+	}
+
+	return &ConnectionPool{
+		db:       db,
+		connChan: connChan,
+	}, nil
+}
+
+func (c *ConnectionPool) Take() *sql.Conn {
+	return <-c.connChan
+}
+
+func (c *ConnectionPool) Put(conn *sql.Conn) {
+	c.connChan <- conn
+}
+
+func benchmarkWithPool() {
+	pool, err := NewConnectionPool(connStr)
+	if err != nil {
+		log.Fatalf("error creating connection pool: %v", err)
+	}
 
 	var wg sync.WaitGroup
-	for range 500 {
+	for range 20 {
 		wg.Go(func() {
-			rows, err := db.Query("SELECT 1")
+			conn := pool.Take()
+			defer pool.Put(conn)
+			rows, err := conn.QueryContext(context.Background(), "SELECT 1")
 			if err != nil {
 				log.Fatalf("error executing query: %v", err)
 			}
