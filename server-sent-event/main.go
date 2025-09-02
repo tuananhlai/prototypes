67eadbf1351f93278cea05e6ee0bf9ba9040e98e
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-09-02 15:38:46 +0700

    minor changes

--- server-sent-event/main.go
+++ server-sent-event/main.go
@@ -14,7 +14,6 @@ const (
 func sse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
 
 	flusher, ok := w.(http.Flusher)
 	if !ok {
