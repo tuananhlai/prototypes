3756552c1ea7f5280b045ebee2db223655a3fb11
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-10-28 18:08:13 +0900

    feat/websocket-scale: finish implementation

--- websocket-scale/main.go
+++ websocket-scale/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"slices"
@@ -16,18 +18,48 @@ func main() {
 	flag.IntVar(&port, "p", 8080, "port to listen on")
 	flag.Parse()
 
+	globalCtx := context.Background()
+
 	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
+	broadcastService := NewBroadcastService()
+	broadcastController := NewBroadcastController(broadcastService, rdb)
 
-	log.Printf("listening on port %d", port)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	mux.Handle("/ws", broadcastController)
+
+	go func() {
+		broadcastController.StartSubscriber(globalCtx)
+	}()
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
+		log.Fatalf("error starting http server")
+	}
 }
 
 type BroadcastController struct {
-	service          *BroadcastService
+	broadcastService *BroadcastService
 	upgrader         websocket.Upgrader
 	rdb              *redis.Client
 	redisChannelName string
+}
+
+func NewBroadcastController(broadcastService *BroadcastService, rdb *redis.Client) *BroadcastController {
+	return &BroadcastController{
+		broadcastService: broadcastService,
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin:     func(r *http.Request) bool { return true },
+		},
+		rdb:              rdb,
+		redisChannelName: "broadcast-channel",
+	}
 }
 
 func (b *BroadcastController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -37,8 +69,8 @@ func (b *BroadcastController) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	b.service.AddConnection(conn)
-	defer b.service.RemoveConnection(conn)
+	b.broadcastService.AddConnection(conn)
+	defer b.broadcastService.RemoveConnection(conn)
 
 	for {
 		_, message, err := conn.ReadMessage()
@@ -46,7 +78,31 @@ func (b *BroadcastController) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 			log.Println("error reading message:", err)
 			break
 		}
+
+		err = b.publishMessage(context.Background(), string(message))
+		if err != nil {
+			log.Println("error publishing message:", err)
+		}
 	}
+}
+
+func (b *BroadcastController) StartSubscriber(ctx context.Context) {
+	pubsub := b.rdb.Subscribe(ctx, b.redisChannelName)
+	defer pubsub.Close()
+
+	var err error
+
+	ch := pubsub.Channel()
+	for msg := range ch {
+		err = b.broadcastService.Broadcast([]byte(msg.Payload))
+		if err != nil {
+			log.Println("error broadcasting message:", err)
+		}
+	}
+}
+
+func (b *BroadcastController) publishMessage(ctx context.Context, msg string) error {
+	return b.rdb.Publish(ctx, b.redisChannelName, msg).Err()
 }
 
 type BroadcastService struct {
