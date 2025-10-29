7d25161a946ff589435500c79c5dde6e0f1babdc
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-10-29 10:10:59 +0900

    feat/fair-multithreaded: init

--- fair-multithreaded/main.go
+++ fair-multithreaded/main.go
@@ -1,45 +1,114 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
+const (
+	fair   = "fair"
+	unfair = "unfair"
+)
+
+// go run main.go fair
+// go run main.go unfair
 func main() {
+	flag.Parse()
+	processingStyle := flag.Arg(0)
+
 	numTasks := 100
+	// A list of integers representing the time it takes to process a task.
+	// The time it takes to process tasks[i] is i + 1 milliseconds.
 	tasks := make([]int, 0, numTasks)
 
 	for i := range numTasks {
-		tasks = append(tasks, i)
+		tasks = append(tasks, i+1)
 	}
 
+	numWorkers := 10
+
+	switch processingStyle {
+	case fair:
+		processTasksFair(tasks, numWorkers)
+	case unfair:
+		processTasksUnfair(tasks, numWorkers)
+	default:
+		log.Fatal("invalid processing style. usage: go run main.go fair|unfair")
+	}
 }
 
-func processTasksUnfair(tasks []int) {
+// Process the given list of tasks in an unfair manner. Each worker is assigned
+// an equal number of tasks to process, regardless of how long the tasks takes to complete.
+func processTasksUnfair(tasks []int, numWorkers int) {
+	log.Println("Processing tasks unfairly...")
 	var wg sync.WaitGroup
 
 	numTasks := len(tasks)
-	numWorker := 10
 
-	numTaskPerWorker := numTasks / numWorker
+	numTaskPerWorker := numTasks / numWorkers
+	remainer := numTasks % numWorkers
 
-	var startIdx, endIdx int
-	for i := range numWorker {
+	var currentIdx int
+	for i := range numWorkers {
 		wg.Add(1)
-		startIdx = i * numTaskPerWorker
-		endIdx = min((i+1)*numTaskPerWorker, numTasks)
+		size := numTaskPerWorker
+		if i < remainer {
+			size++
+		}
+		endIdx := currentIdx + size
 
-		go func(start int, end int) {
+		go func(threadID int, start int, end int) {
 			startTime := time.Now()
 			for i := start; i < end; i++ {
 				processTask(tasks[i])
 			}
-			log.Println("Processed from", start, "to", end, "in", time.Since(startTime))
-		}(startIdx, endIdx)
+			log.Println("Thread", threadID, "finished in", time.Since(startTime))
+			wg.Done()
+		}(i, currentIdx, endIdx)
+
+		currentIdx = endIdx
 	}
+
+	wg.Wait()
 }
 
-func processTask(task int) {
-	time.Sleep(time.Millisecond * time.Duration(task))
+// Process the given list of tasks in a fair manner. Each worker continues to process
+// the latest unprocessed tasks until all tasks are finished.
+func processTasksFair(tasks []int, numWorkers int) {
+	log.Println("Processing tasks fairly...")
+
+	var idx atomic.Int64
+	var wg sync.WaitGroup
+
+	numTasks := int64(len(tasks))
+
+	for i := range numWorkers {
+		wg.Add(1)
+		go func(threadID int) {
+			startTime := time.Now()
+
+			for {
+				// use a single atomic operation to add + retrieve current value to prevent race condition.
+				currentIdx := idx.Add(1) - 1
+				if currentIdx >= numTasks {
+					break
+				}
+				task := tasks[currentIdx]
+				processTask(task)
+			}
+
+			log.Println("Thread", threadID, "finished in", time.Since(startTime))
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+// Simulate a task that takes a given amount of time (ms) to complete.
+func processTask(timeMs int) {
+	time.Sleep(time.Millisecond * time.Duration(timeMs))
 }
